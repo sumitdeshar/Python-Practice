0cc696ce4bc54a94c6801e8110074524aa295071
Sumit Deshar <sumeetdsr@gmail.com>
2025-01-15 08:40:18 +0545

    unit testing using pytest

---
+++ UnitTesting/tests/test_person.py
@@ -0,0 +1,32 @@
+from pytest_tryout.person import Person
+import pytest
+
+@pytest.fixture()
+def person():
+    return Person('Ethan Henderson', 24, jobs=['Software Engineer'])
+
+
+def test_init(person : Person):
+    assert person.name == 'Ethan Henderson'
+    assert person.age == 24
+    assert person.jobs == ['Software Engineer']
+    
+    
+def test_forename(person : Person):
+    assert person.forename == 'Ethan'
+    
+def test_surname(person : Person):
+    assert person.surname == 'Henderson'
+    
+def test_no_surname(person : Person):
+    person.name = 'Ethan'
+    assert not person.surname
+    
+def test_celebrate_birthday(person : Person):
+    person.celebrate_birthday()
+    assert person.age ==25
+    
+def test_add_job(person : Person):
+    person .add_job('Backend Developer')
+    assert person.jobs == ['Software Engineer', 'Backend Developer']
+    
